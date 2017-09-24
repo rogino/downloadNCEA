4df64c44b5f26b9bf351a41962c2b4f88983d389
rogino <rio.ogino@gmail.com>
2017-09-25 12:30:26 +1300

    Stashing or something- I don't understand github

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 let download = require("download");
 let inquirer = require("inquirer");
 let fs = require("fs");
-// let mv = require("mv");
-//let pdf2json = require("pdf2json");
-let baseFolder = "C:/Users/Rio/OneDrive - Christchurch Boys' High School/School Work/Year 12/NCEAPastPapers/"; 
-
+let baseDirectory = "C:/Users/Rio/OneDrive - Christchurch Boys' High School/School Work/Year 12/NCEAPastPapers/";
+inquirer.registerPrompt("directory", require("inquirer-directory"));
 
 
 let reg = {
   file: new RegExp(/\.\w+$/g),
-  directoryWindows: new RegExp(/^\s*[^\>\<\\\"\/\|\?\*\.]*\S*$/g),
+  directoryWindows: new RegExp(/^\s*[^\>\<\\\"\/\|\?\*\.]+\s*$/g),
   integer: new RegExp(/^\s*\d+\s*$/),
   year_2_4: new RegExp(/(?:^\s*(?:\d{2}|\d{4})?\s*$)?/)
 };
@@ -20,8 +18,7 @@ let name = {
   capitalizeWord: (string, justTheFirstWord) => {
     if (justTheFirstWord) {
       return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-    else {
+    } else {
       let arr = string.split(" "); //Only works with spaces. Consider looping with reg.exec to capitalize and add the matched whitespace character
       let str = "";
       for (let i of arr) {
@@ -46,114 +43,154 @@ let name = {
 };
 
 
-inquirer.registerPrompt("directory", require("inquirer-directory"));
-
-inquirer.prompt([{
-    type: "input",
-    name: "standardCode",
-    message: "Standard code: ",
-    validate: val => (val.match(reg.integer)) ? true : "Enter numerical standard code"
-  }, {
-    type: "input",
-    name: "standardName",
-    message: "Name of standard (e.g. Mechanics): ",
-    validate: val => val.match(reg.folderWindows) ? true: "Please enter a valid folder name"
-  }, {
-    type: "input",
-    name: "year",
-    message: "Year of exam (or nothing for all from 2012): ",
-    validate: val => (val.match(reg.year_2_4) ? true : "Enter 2/4 digit year, or nothing for all years")
-  }, {
-    type: "list",
-    name: "downloadOptions",
-    message: "Download:",
-    choices: [{
-      name: "Exam paper and marking schedule",
-      value: ["exam", "marking"]
+let askUser = () => {
+  inquirer.prompt([{
+      type: "input",
+      name: "standardCode",
+      message: "Standard code: ",
+      validate: val => (val.match(reg.integer)) ? true : "Enter numerical standard code"
     }, {
-      name: "Exam Paper, marking schedule and resource booklet",
-      value: ["exam", "marking", "resource"]
+      type: "input",
+      name: "standardName",
+      message: "Name of standard (e.g. Mechanics): ",
+      validate: val => val.match(reg.folderWindows) ? true : "Please enter a valid directory name"
     }, {
-      name: "Exam paper, marking schedule, resource booklet and excellence exemplar",
-      value: ["exam", "marking", "resource", "excellence"]
+      type: "input",
+      name: "year",
+      message: "Year of exam (or nothing for all from 2012): ",
+      validate: val => (val.match(reg.year_2_4) ? true : "Enter 2/4 digit year, or nothing for all years")
     }, {
-      name: "Everything",
-      value: ["exam", "marking", "resource", "notachieved", "achieved", "merit", "excellence"]
-    }]
-  }, {
-    type: "list",
-    name: "newSaveLocation",
-    message: `Download to '${baseFolder}'?`,
-    choices: [{
-      name: "Yes",
-      value: true
+      type: "list",
+      name: "downloadOptions",
+      message: "Download:",
+      choices: [{
+        name: "Exam paper and marking schedule",
+        value: ["exam", "marking"]
+      }, {
+        name: "Exam Paper, marking schedule and resource booklet",
+        value: ["exam", "marking", "resource"]
+      }, {
+        name: "Exam paper, marking schedule, resource booklet and excellence exemplar",
+        value: ["exam", "marking", "resource", "excellence"]
+      }, {
+        name: "Everything",
+        value: ["exam", "marking", "resource", "notachieved", "achieved", "merit", "excellence"]
+      }]
     }, {
-      name: "No",
-      value: false
-    }]
-  }
-  
-]).then(val => {
-
-  let func = (options, dir) => {
-    //Because async/promises
-    let newDir = directorySearch(dir, 
-      folder => (folder.includes(val.standardCode) || folder.toLowerCase().includes(val.standardName.toLowerCase())), 3);
-    //newDir is the full path of the new directory
-    if (newDir) {
-      console.log(`Automatically detected folder, ${name.concatDir(newDir)}`);
-      parseInput(val, newDir);
+      type: "list",
+      name: "newSaveLocation",
+      message: `Download to '${baseDirectory}'?`,
+      choices: [{
+        name: "Yes",
+        value: true
+      }, {
+        name: "No",
+        value: false
+      }]
     }
-    else {
-      console.log(`Downloading to folder, ${name.concatDir(dir, name.standardFolder(val.standardCode, val.standardName))}`);
-      parseInput(val, name.concatDir(dir, name.standardFolder(val.standardCode, val.standardName)));
-    }
-  };
-  
-  if (!val.newSaveLocation) {
-    inquirer.prompt([{
-      type: "directory",
-      name: "saveLocation",
-      message: "Base folder to save in: ",
-      basePath: baseFolder
-    }]).then(newDir => {
-      func(val, name.concatDir(baseFolder, newDir.saveLocation));
-    }
-    ).catch(err => {
-      throw new Error(err);
-    });
-  }
-  else {
-    func(val, baseFolder);
-  }
-}).catch(err => {
-  throw new Error(err);
-});
 
+  ]).then(val => {
+    let func = (options, dir) => {
+      //Because async/promises, and as both val.newSaveLocation values will require this function
+      console.log(`Searching directory: ${dir}`);
+      let newDir = directorySearch(dir,
+        directory => (directory.includes(val.standardCode) || directory.toLowerCase().includes(val.standardName.toLowerCase())), 3);
+      //newDir is the full path of the new directory
+      if (newDir) {
+        console.log(`Automatically detected directory, ${name.concatDir(newDir)}`);
+        parseInput(val, newDir);
+      } else {
+        console.log(`Downloading to directory, ${name.concatDir(dir, name.standardFolder(val.standardCode, val.standardName))}`);
+        parseInput(val, name.concatDir(dir, name.standardFolder(val.standardCode, val.standardName)));
+      }
+    };
 
-//console.log(directorySearch("C:/Users/Rio/OneDrive - Christchurch Boys' High School/School Work/Year 12/NCEAPastPapers/",str => (str.includes("91171") || str.toLowerCase().includes("mechanics")), 3));
+    if (!val.newSaveLocation) {
+      //If user wants to save in new location
+      inquirer.prompt([{
+        type: "directory",
+        name: "saveLocation",
+        message: "Base directory to save in: ",
+        basePath: baseDirectory
+      }]).then(newDir => {
+        func(val, name.concatDir(baseDirectory, newDir.saveLocation));
+      }).catch(err => {
+        throw new Error(err);
+      });
+    } else {
+      //Save in default location
+      func(val, baseDirectory);
+    }
+  }).catch(err => {
+    throw new Error(err);
+  });
+};
+
+console.log(directorySearch("C:/Users/Rio/", val => val.includes("photo"), 4));
+
 
 function directorySearch(directory, directoryFilter, maxSearchDepth = 1) {
   //Directory filter accepts the directory name and full path name as arguments
   //maxSearchDepth is the levels it will travel. 0 for no limit
   directory = name.concatDir(directory); //clean up backslashes
-  console.log(`Searching directory: ${directory}`);
   let dirArr = directory.split("/"); //create array of directories
   deepest = dirArr[dirArr.length - 2]; //The name of the 'deepest' directory. -2 as string ends with a /, so will be an empty string. Thus, the second to last one.
 
-
   if (directoryFilter(deepest, directory)) return directory; //Check if the current directory matches the function
- 
+
   else {
     //Recursive search function
     let search = (directory, depth, maxDepth) => {
       //Directory: will scan contents of the directory. Depth: current depth, starts at 1. Max depth: how deep you can go. 0 is no limit. 
-
+      let folders;
       try {
-        let folders = fs.readdirSync(directory).filter(name => !name.match(reg.file));
+        fs.readdirSync(directory);
+      }
+      catch(err) {
+        if (!(err.code === "ENOTDIR" || err.code === "EPERM")) console.log(err);
+        return false;
+      }
+
+      if (folders) {
+        try {
+          folders = folders.filter(name => fs.statSync(name).isDirectory());
+        }
+        catch(err) {
+          if (!(err.code === "ENOTDIR" || err.code === "EPERM")) console.log(err);
+        }
+      }
+      console.log(folders);
+
+      //Get list of stuff in directory, and remove files from it
+      for (let i of folders) {
+        //Look through each subdirectory
+        
+        let dirPath = name.concatDir(directory, i); //Full path of the subdirectory
+        if (directoryFilter(i, dirPath)) return dirPath;
+        //Return if matches 
+
+        else if (depth < maxDepth || depth === 0) {
+          //Go deeper: looping through each subdirectory until maximum depth reached
+          let val = search(dirPath, depth + 1, maxDepth);
+          //Return it only if it has successfully gotten a match
+          if (val) return val;
+        }
+      }
+      /*
+      try {
+        let folders = fs.readdirSync(directory).filter(name => {
+          fs.statSync(name, (err, stats) => {
+            if (err) {
+              if (err.code !== "ENOENT") console.log("!"); //Log non-ENOENT errors
+              return false;
+            }
+            return stats.isDirectory(); 
+          }); //Remove files from directory
+        });//.filter(name => !name.match(reg.file));
         //Get list of stuff in directory, and remove files from it
         for (let i of folders) {
           //Look through each subdirectory
+          
           let dirPath = name.concatDir(directory, i); //Full path of the subdirectory
           if (directoryFilter(i, dirPath)) return dirPath;
           //Return if matches 
@@ -163,19 +200,15 @@ function directorySearch(directory, directoryFilter, maxSearchDepth = 1) {
             let val = search(dirPath, depth + 1, maxDepth);
             //Return it only if it has successfully gotten a match
             if (val) return val;
-
           }
         }
-      }
-      catch(err) {
+      } catch (err) {
         if (!(err.code === "ENOTDIR" || err.code === "EPERM")) {
-          console.log(err);
+          //console.log(err);
         }
-        //Errors with ENOTDIR (not directory) or EPERM (not permitted) etc. 
-      }
+      }*/
     };
     let dirName = search(directory, 1, maxSearchDepth); //Start depth at 0
-    //console.log(dirName);
     if (dirName) return dirName;
   }
 
@@ -185,8 +218,6 @@ function directorySearch(directory, directoryFilter, maxSearchDepth = 1) {
 
 
 function parseInput(result, directory) {
-  // console.log(result);
-  // console.log(`Downloading to ${directory}`);
   let year = parseInt(result.year, 10);
   let standardCode = parseInt(result.standardCode, 10);
 
@@ -224,7 +255,7 @@ let linkAnswers = (year, standardCode, standardName) => {
     standardCode: standardCode,
     standardName: standardName,
     exam: `${baseLink}/exams/${year}/${standardCode}-exm-${year}.pdf`,
-    marking: `${baseLink}/schedules/${year}/${standardCode}-ass-${year}.pdf`, 
+    marking: `${baseLink}/schedules/${year}/${standardCode}-ass-${year}.pdf`,
     notachieved: `${baseLink}/examplars/${year}/${standardCode}-exp-${year}-notachieved.pdf`,
     achieved: `${baseLink}/examplars/${year}/${standardCode}-exp-${year}-achieved.pdf`,
     merit: `${baseLink}/examplars/${year}/${standardCode}-exp-${year}-merit.pdf`,
@@ -236,29 +267,30 @@ let linkAnswers = (year, standardCode, standardName) => {
 
 
 
-let downloadPaper = (folder, object, toDownload) => {
-  console.log(`Downloading ${object.year} ${object.standardCode} papers to ${folder}`);
+let downloadPaper = (directory, object, toDownload, overwrite = false) => {
+  console.log(`Downloading ${object.year} ${object.standardCode} papers to ${directory}`);
 
-  for (let i = toDownload.length - 1; i >= 0; i--) {
-    if (fs.existsSync(name.concatDir(folder, name.standardFile(object.standardCode, object.year, toDownload[i])))) {
-      //Checks if the file that is going to be created actually exists
-      console.log(`'${name.standardFile(object.standardCode, object.year, toDownload[i])}' already exists: skipping download`);
-      toDownload = toDownload.splice(i, 1); //Remove element from the array
-      //Going in reverse, so removing an element will have no effect on the ones before it
+  if (!overwrite) {
+    for (let i = toDownload.length - 1; i >= 0; i--) {
+      if (fs.existsSync(name.concatDir(directory, name.standardFile(object.standardCode, object.year, toDownload[i])))) {
+        //Checks if the file that is going to be created actually exists
+        console.log(`'${name.standardFile(object.standardCode, object.year, toDownload[i])}' already exists: skipping download`);
+        toDownload = toDownload.splice(i, 1); //Remove element from the array
+        //Going in reverse, so removing an element will have no effect on the ones before it
+      }
     }
   }
 
   return 0;
-  Promise.all(toDownload.map(i=>download(object[i], folder, {
-      filename: name.standardFile(object.standardCode, object.year , i)
-    }))).then(val=> {
-      console.log(`Downloads for ${object.year} ${object.standardCode} finished`);
-    }).catch(err=>{
-      if (err.name == "HTTPError" && err.statusCode == 404) {
-        console.log(`404 (File not found) error downloading '${err.url}'`);
-      }
-      else {
-        console.log(`An error occured: ${err}`);
-      }
-    });
+  Promise.all(toDownload.map(i => download(object[i], directory, {
+    filename: name.standardFile(object.standardCode, object.year, i)
+  }))).then(val => {
+    console.log(`Downloads for ${object.year} ${object.standardCode} finished`);
+  }).catch(err => {
+    if (err.name == "HTTPError" && err.statusCode == 404) {
+      console.log(`404 (File not found) error downloading '${err.url}'`);
+    } else {
+      console.log(`An error occured: ${err}`);
+    }
+  });
 };
